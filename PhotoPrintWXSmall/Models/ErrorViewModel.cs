b02204dc3ce2748b1a01ffe164b99fdab040922e
Alair <alaireileen@outlook.com>
2017-12-05 15:22:21 +0800

    Add project files.

---
+++ PhotoPrintWXSmall/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhotoPrintWXSmall.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
